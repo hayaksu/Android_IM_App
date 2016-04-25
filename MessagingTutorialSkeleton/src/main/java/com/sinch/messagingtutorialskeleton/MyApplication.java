edcaad0cbb35517fe315ec1c91f84b2fc5938ee9
Sakurami-m <blacky.rose.m@gmail.com>
2016-04-25 21:04:15 +0300

    Update MyApplication.java

--- MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/MyApplication.java
+++ MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/MyApplication.java
@@ -1,7 +1,7 @@
 package com.sinch.messagingtutorialskeleton;
 
 /**
- * Created by Haya on 10/27/2015.
+ * Created by Haya on 20/3/2016.
  */
 import android.app.Application;
 import android.util.Log;
@@ -22,6 +22,7 @@ public class MyApplication extends Application {
         super.onCreate();
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "6zuqfy2rPtBTFIhHcrQy1TbuCwXLvhJ1mcFZEA4R", "iK6g3dGw9Am5XMZo8O7nph3Lve5bGq8XiTQ8OtD8");
+        
         // Reading from TransDatabase
         List<ParseObject> textMSGList=null;
         ParseQuery<ParseObject> query = ParseQuery.getQuery("TransDatabase");//TransDatabase
@@ -33,15 +34,16 @@ public class MyApplication extends Application {
 
         //array to covert list to array
         try {
+
             String[] array = new String[textMSGList.size()];
             int g=0;
+
             //add the data in the list to the array
             for (ParseObject comment : textMSGList) {
+
                 array[g]=comment.get("FilteredMsg").toString();
-                // Log.i("testing ",array[g]);
                 g++;
             }
-            //  Apriori.main(array);
 
         }
         catch (Exception e)
@@ -49,58 +51,6 @@ public class MyApplication extends Application {
             Toast.makeText(this, "تأكد من اتصالك بالانترنت", Toast.LENGTH_LONG).show();
 
         }
-
- /*       try {
-         System.out.println("start");
-            ParseQuery<ParseObject> query1 = ParseQuery.getQuery("ParseMessage");
-            System.out.println("start2");
-
-        List<ParseObject> Objectslist = null;
-            System.out.println("start4");
-
-            Objectslist = query1.find();
-
-        for (int index = 40; index < 53; ++index) {
-            String str = Objectslist.get(index).getString("messageText");
-            System.out.println("start5");
-
-            PrepareData.AddaConversation(str);
-        }
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }*/
-
-
-/*        //array to covert list to array
-        String [] array = new String[textMSGList.size()];
-        int g=0;
-        //add the data in the list to the array
-        for (ParseObject comment : textMSGList) {
-            array[g]=comment.get("FilteredMsg").toString();
-         Log.i("testing ",array[g]);
-            g++;
-        }
-
-
-String [] test ={"1 2 3 4",
-        "1 5 6 7",
-        "1 8 9 10",
-        "1 2 3 4",
-        "1 2 3 4",
-        "10 11 12 13"};
-        try {
-            Log.i("before ","before apriori");
-            Apriori.main(array);
-            Log.i("after ", "after apriori");
-
-        } catch (Exception e) {
-            Log.i("try exception", "try exception");
-
-            e.printStackTrace();
-        }
-
-*/
 
     }
 }
